566802c1de928c6ad9e2c84676bd8b92ce22aa11
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-09 00:51:21 -0300

    feat: check user booking modality in hotel reservation

--- src/services/client/hotelReservation.ts
+++ src/services/client/hotelReservation.ts
@@ -1,9 +1,13 @@
 import ConflictError from "@/errors/ConflictError";
 import HotelReservation from "@/entities/HotelReservation";
 import Hotel from "@/entities/Hotel";
+import Booking from "@/entities/Booking";
+import InvalidBookingError from "@/errors/InvalidBooking";
 
 export async function ReserveHotelRoom(hotelId: number, roomId: number, userId: number) {
   const hotel = await Hotel.getWithSpecifiedRoomAndSpecifiedReservation(hotelId, roomId, userId);
+  const booking = await Booking.findOne({ where: { userId } });
+  if(booking===undefined || booking.lodge.type === "Sem Hotel" || booking.lodgeId === null) throw new InvalidBookingError();
   if(hotel.rooms[0].freeVacancies() === 0) throw new ConflictError("room is full");
   const hotelReservation = hotel.hotelReservations[0];
   const newHotelReservation = HotelReservation.create({ hotelId, roomId, userId });
--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -2,15 +2,20 @@ import supertest from "supertest";
 import httpStatus from "http-status";
 
 import app, { init } from "../../src/app";
-import Session from "../../src/entities/Session";
 import { clearDatabase, endConnection } from "../utils/database";
 import { createBasicSettings } from "../utils/app";
 import { CreateSession } from "../factories/userFactory";
 import { createHotel } from "../factories/hotelFactory";
 import Room from "../../src/entities/Room";
+import { createBooking, createDataAndReturnToken } from "../factories/bookingFactory";
+import  Session  from "../../src/entities/Session";
+import  User  from "../../src/entities/User";
 
 const agent =  supertest(app);
-let session: Session; 
+let userData: {
+  session: Session;
+  user: User;
+}; 
 beforeAll(async () => {
   await init();
 });
@@ -18,7 +23,7 @@ beforeAll(async () => {
 beforeEach(async () => {
   await clearDatabase();
   await createBasicSettings();
-  session = await CreateSession();
+  userData = await CreateSession();
 });
 
 afterAll(async () => {
@@ -33,7 +38,7 @@ describe("GET /hotels", () => {
   });
   it("should return an array with all hotels for valid token", async () => {
     const hotel = await createHotel();
-    const response = await agent.get("/hotels").set("authorization", `Bearer ${session.token}`);
+    const response = await agent.get("/hotels").set("authorization", `Bearer ${userData.session.token}`);
 
     expect(response.body).toEqual(expect.arrayContaining([{
       ...hotel,
@@ -43,7 +48,7 @@ describe("GET /hotels", () => {
   });
   it("should return status 200 for valid token", async () => {
     await createHotel();
-    const response = await agent.get("/hotels").set("authorization", `Bearer ${session.token}`);
+    const response = await agent.get("/hotels").set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toEqual(httpStatus.OK);
   });
 });
@@ -55,47 +60,64 @@ describe("GET /hotels/:id", () => {
   });
   it("should return an array with all hotels for valid token", async () => {
     const hotel = await createHotel();
-    const response = await agent.get(`/hotels/${hotel.id}`).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.get(`/hotels/${hotel.id}`).set("authorization", `Bearer ${userData.session.token}`);
 
     expect(response.body).toEqual(hotel);
   });
   it("should return status 200 for valid token", async () => {
     const hotel = await createHotel();
-    const response = await agent.get(`/hotels/${hotel.id}`).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.get(`/hotels/${hotel.id}`).set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toEqual(httpStatus.OK);
   });
 });
 
 describe("POST /hotels/:hotelId/rooms/:roomId", () => {
+  let data: {
+    token: string;
+    modalityIds: number[];
+    lodgeIds: number[];
+  };
+  beforeEach(async () => {
+    data = await createDataAndReturnToken();
+  });
   it("should return status 201 for valid params", async () => {
+    await createBooking({ modalityId: data.modalityIds[0], lodgeId: data.lodgeIds[0], userId: userData.user.id, value: 500 });
     const hotel = await createHotel();
-    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toBe(httpStatus.CREATED);
   });
+  it("should incremente ocuppiedVacencies in selected room for valid params", async () => {
+    await createBooking({ modalityId: data.modalityIds[0], lodgeId: data.lodgeIds[0], userId: userData.user.id, value: 500 });
+    const hotel = await createHotel();
+    const oldVacancies = hotel.rooms[0].ocuppiedVacancies;
+    await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
+    const room = await Room.findOne(hotel.rooms[0].id);
+    expect(oldVacancies).toBe(room.ocuppiedVacancies -1);
+  });
+  it("should return status 401 for 'Sem Hotel' lodge", async () => {
+    await createBooking({ modalityId: data.modalityIds[0], lodgeId: data.lodgeIds[1], userId: userData.user.id, value: 500 });
+    const hotel = await createHotel();
+    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
+    expect(response.statusCode).toBe(httpStatus.UNAUTHORIZED);
+  });
   it("should return status 409 for full room", async () => {
+    await createBooking({ modalityId: data.modalityIds[0], lodgeId: data.lodgeIds[0], userId: userData.user.id, value: 500 });
     const hotel = await createHotel();
     const room = hotel.rooms.find(r => r.ocuppiedVacancies === r.roomVacancies);
-    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${room.id}`).send({}).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${room.id}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toBe(httpStatus.CONFLICT);
   });
   it("should return status 404 for invalid hotelId", async () => {
     const hotel = await createHotel();
-    const response = await agent.post(`/hotelReservations/hotels/${hotel.id+1}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.post(`/hotelReservations/hotels/${hotel.id+1}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toBe(httpStatus.NOT_FOUND);
   });
 
   it("should return status 404 for invalid roomId", async () => {
     const hotel = await createHotel();
     const room = hotel.rooms[hotel.rooms.length-1];
-    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${room.id+1}`).send({}).set("authorization", `Bearer ${session.token}`);
+    const response = await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${room.id+1}`).send({}).set("authorization", `Bearer ${userData.session.token}`);
     expect(response.statusCode).toBe(httpStatus.NOT_FOUND);
-  });
-  it("should incremente ocuppiedVacencies in selected room for valid params", async () => {
-    const hotel = await createHotel();
-    const oldVacancies = hotel.rooms[0].ocuppiedVacancies;
-    await agent.post(`/hotelReservations/hotels/${hotel.id}/rooms/${hotel.rooms[0].id}`).send({}).set("authorization", `Bearer ${session.token}`);
-    const room = await Room.findOne(hotel.rooms[0].id);
-    expect(oldVacancies).toBe(room.ocuppiedVacancies -1);
   });
 });
 
