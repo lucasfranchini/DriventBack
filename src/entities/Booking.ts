cea5af557552c3fcfa830f5e37b2b2e632dd22cf
jotabraga <jpmbraga.feg@gmail.com>
2021-09-09 00:58:42 -0300

    fix: request for no booking content

--- src/entities/Booking.ts
+++ src/entities/Booking.ts
@@ -66,7 +66,6 @@ export default class Booking extends BaseEntity {
 
   static async getBooking(userId: number) {
     const booking = await this.findOne({ where: { userId } });
-    if (!booking) throw new Unauthorized();
     return booking;
   }
 }
