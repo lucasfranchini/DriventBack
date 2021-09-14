68be006c4071a5d87d7757841eca5c603419a57b
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-14 13:24:24 -0300

    feat: unit tests for signIn

---
+++ tests/unit/auth.test.ts
@@ -0,0 +1,31 @@
+import { signIn } from "../../src/services/client/auth";
+import User from "../../src/entities/User";
+import { client } from "../../src/app";
+import { promisify } from "util";
+import jwt from "jsonwebtoken";
+
+afterAll(async () => {
+  const quitAsync = promisify(client.quit).bind(client);
+  await quitAsync();
+});
+
+describe("signIn", () => {
+  it("thorws an error for unauthorized user", async () => {
+    jest.spyOn(User, "findByEmailAndPassword").mockImplementationOnce(async () => null);
+    const result = async () => await signIn("", "");
+    await expect(result).rejects.toThrow(Error);
+  });
+  it("thorws an error for unauthorized user", async () => {
+    jest.spyOn(User, "findByEmailAndPassword").mockImplementationOnce(async () => {return { email: "test", id: 1 }  as User;});
+    jest.spyOn(jwt, "sign").mockImplementationOnce(() => "funcionou");
+    const result = await signIn("", "");
+    
+    expect(result).toMatchObject({
+      user: {
+        email: "test",
+        id: 1
+      },
+      token: "funcionou"
+    });
+  });
+});
