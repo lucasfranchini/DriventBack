ad48c5da0bf73c44956d6245795eb23f28dd58f2
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-14 17:28:40 -0300

    feat: send reset password toker to email

--- src/services/client/resetPassword.ts
+++ src/services/client/resetPassword.ts
@@ -1,9 +1,19 @@
 import User from "@/entities/User";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import sgMail from "@sendgrid/mail";
 
 export async function sendToken(email: string) {
   await User.verifyEmail(email);
   const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: 60*60 });
+  const link = `${process.env.FRONT_URL}/resetPassword/${token}`;
+  const mail= {
+    to: email,
+    from: process.env.FROM_EMAIL,
+    subject: "Password change request",
+    text: `Please click on the following link ${link} to reset your password. \n\n 
+      If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+  }; 
+  await sgMail.send(mail);
 }
 
 export async function verifyTokenValidation(token: string) {
