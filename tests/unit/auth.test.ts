952c5b44960fb2a4f88f3f1e29bee2c1d2959487
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-14 14:10:28 -0300

    refactor: change it texts in auth unit test

--- tests/unit/auth.test.ts
+++ tests/unit/auth.test.ts
@@ -15,10 +15,10 @@ describe("signIn", () => {
     const result = async () => await signIn("", "");
     await expect(result).rejects.toThrow(Error);
   });
-  it("thorws an error for unauthorized user", async () => {
+  it("returns an object for correct path", async () => {
     jest.spyOn(User, "findByEmailAndPassword").mockImplementationOnce(async () => {return { email: "test", id: 1 }  as User;});
     jest.spyOn(jwt, "sign").mockImplementationOnce(() => "funcionou");
-    jest.spyOn(client, "set").mockImplementationOnce(() => {return true} );
+    jest.spyOn(client, "set").mockImplementationOnce(() => {return true;} );
     const result = await signIn("", "");
     
     expect(result).toMatchObject({
